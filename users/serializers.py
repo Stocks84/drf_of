2b67368cb9d0b5151e6555db1b431d7888ebafb5
William Stocks <willstocks@me.com>
2024-10-10 15:05:31 +0000

    Add UserProfileSerializer to serializers.py

---
+++ users/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import UserProfile
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = [
+            'id', 'username', 'first_name', 'last_name', 'bio', 
+            'favorite_drink', 'location', 'profile_image', 
+            'date_of_birth', 'date_joined', 'is_active', 'is_staff'
+        ]
+        read_only_fields = ['date_joined', 'is_active', 'is_staff']
