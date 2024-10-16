707d5097376fbaab77fdc3ca22ef70225525950d
William Stocks <willstocks@me.com>
2024-10-16 15:54:00 +0000

    Add Game, Comment, Like & permission views to games

--- games/views.py
+++ games/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Game, Comment, Like
+from .serializers import GameSerializer, CommentSerializer, LikeSerializer
+
 
 # Create your views here.
+
+class CreatorOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.creator == request.user
+
+class GameViewSet(viewsets.ModelViewSet):
+    queryset = Game.objects.all()
+    serializer_class = GameSerializer
+    permission_classes = [CreatorOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(creator=self.request.user)
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class LikeViewSet(viewsets.ModelViewSet):
+    queryset = Like.objects.all()
+    serializer_class = LikeSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
